b96b6e414d50307a66b3ff7e6afab87021adc5ca
evgeny <evgeny@158.160.8.209>
2024-09-24 00:07:43 +0700

    css

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/styles.scss";`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@styles': resolve(__dirname, './src/assets'),
+      '@images': resolve(__dirname, './src/images'),
+    },
+    extensions: ['.js', '.json', '.svg', '.scss'],
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+});
