b08fabbde6a046d50ae6bf1c22b3fd82ef86ff41
evgeny <evgeny@158.160.8.209>
2024-09-23 13:34:35 +0700

    сделал тестовое задание

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,9 @@
-
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-    ],
+  plugins: [],
 
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData:
-                    "@import './src//styles.scss';",
-            },
-        },
-    },
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, './src'),
-            '@styles': resolve(__dirname, './src/Assets/stylesheets'),
-            '@images': resolve(__dirname, './src/images'),
-            '@fonts': resolve(__dirname, './src/fonts'),
-        },
-        extensions: [ '.js', '.ts', '.json', '.vue', '.svg' ],
-    },
-    envPrefix: 'APP_',
+  envPrefix: 'APP_',
 });
