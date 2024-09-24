f1adbf500d10553b0ea6cee8c0135910d9a6d323
evgeny <evgeny@158.160.8.209>
2024-09-24 19:44:21 +0700

    сделал бургер меню и кастомный селект

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
+// range
 const rangeInput = document.getElementById('range');
 const rangeValue = document.getElementById('range-value');
 
 rangeInput.addEventListener('input', function () {
   rangeValue.textContent = `${rangeInput.value}%`;
+});
+
+// burger
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('.header__burger');
+  const nav = document.querySelector('.header__links');
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    nav.classList.toggle('active');
+  });
+});
+
+// select
+const customSelectHeader = document.querySelector('.custom-select__header');
+const customSelectOptions = document.querySelector('.custom-select__options');
+const customSelectSelected = document.querySelector('.custom-select__selected');
+
+customSelectHeader.addEventListener('click', () => {
+  customSelectOptions.classList.toggle('active');
+  customSelectHeader.classList.toggle('custom-select--open');
+});
+
+document.querySelectorAll('.custom-select__option').forEach((option) => {
+  option.addEventListener('click', (e) => {
+    customSelectSelected.textContent = e.target.textContent;
+    customSelectOptions.classList.remove('active');
+    customSelectHeader.classList.remove('custom-select--open');
+  });
 });
