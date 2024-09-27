ba139a729861e68d3e2e6f9108067cd92e455f3b
evgeny <evgeny@158.160.8.209>
2024-09-27 16:05:58 +0700

    добавил модальное окно регистрации

--- src/index.js
+++ src/index.js
@@ -34,3 +34,63 @@ document.querySelectorAll('.custom-select__option').forEach((option) => {
     customSelectHeader.classList.remove('custom-select--open');
   });
 });
+
+// modal
+const modalAuth = document.querySelector('.popup');
+const btnAuth = document.querySelector('.header__signin');
+const btnPopupClose = document.querySelector('.popup__button-close');
+
+btnAuth.addEventListener('click', () => {
+  modalAuth.style.display = 'flex';
+});
+
+btnPopupClose.addEventListener('click', () => {
+  modalAuth.style.display = 'none';
+});
+
+btnPopupClose.addEventListener('click', () => {
+  modalAuth.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target === popup) {
+    modalAuth.style.display = 'none';
+  }
+});
+window.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    modalAuth.style.display = 'none';
+  }
+});
+
+//reg
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const submit = document.querySelector('#submit');
+
+const users = {};
+
+function User(name, email, password) {
+  this.name = name;
+  this.email = email;
+  this.password = password;
+}
+
+function createId(users) {
+  return Object.keys(users).length;
+}
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  const nameUser = name.value;
+  const emailUser = email.value;
+  const passwordUser = password.value;
+
+  const user = new User(nameUser, emailUser, passwordUser);
+
+  const userId = 'User' + createId(users);
+  users[userId] = user;
+
+  console.log(users);
+});
